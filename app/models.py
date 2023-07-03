f26abb6bbb03a54d09c2035b89319701fa38215a
Sam-Banankhu <sambanankhumhango@gmail.com>
2023-07-03 09:49:38 +0200

    working with database

--- app/models.py
+++ app/models.py
@@ -6,27 +6,57 @@ class District(db.Model):
     code = db.Column(db.String(255))
     region = db.Column(db.String(255))
 
+    def populate_from_csv(self, row):
+        self.name = row[0]
+        self.code = row[1]
+        self.region = row[2]
+
+
 class ResidentialArea(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     district_id = db.Column(db.Integer, db.ForeignKey('district.id'))
     name = db.Column(db.String(255))
+
+    def populate_from_csv(self, row):
+        self.district_id = int(row[0])
+        self.name = row[1]
+
 
 class TraditionalAuthority(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     district_id = db.Column(db.Integer, db.ForeignKey('district.id'))
 
+    def populate_from_csv(self, row):
+        self.name = row[0]
+        self.district_id = int(row[1])
+
+
 class Village(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     village_name = db.Column(db.String(255))
     district_id = db.Column(db.Integer, db.ForeignKey('district.id'))
+
+    def populate_from_csv(self, row):
+        self.village_name = row[0]
+        self.district_id = int(row[1])
+
 
 class Constituency(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     district_id = db.Column(db.Integer, db.ForeignKey('district.id'))
 
+    def populate_from_csv(self, row):
+        self.name = row[0]
+        self.district_id = int(row[1])
+
+
 class Ward(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     constituency_id = db.Column(db.Integer, db.ForeignKey('constituency.id'))
+
+    def populate_from_csv(self, row):
+        self.name = row[0]
+        self.constituency_id = int(row[1])
